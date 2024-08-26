b21d70a44bbed56f9a0392beaf07a009c5c72594
Daniel Moos <dmoos@ripple.com>
2024-08-26 12:01:07 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +7,11 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let response = String::from("HTTP/1.1 200 OK\r\n\r\n");
+
+                _stream.write_all(response.as_bytes()).expect("could not send response");
             }
             Err(e) => {
                 println!("error: {}", e);
